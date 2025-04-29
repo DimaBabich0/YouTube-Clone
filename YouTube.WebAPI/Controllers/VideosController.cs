ecf9a7f40bc0d87fa7253e0ad89138173e9f3df6
Maks <super_1209@ukr.net>
2025-04-30 01:51:33 +0300

    add DetailVideo, VideoControls and Video.js in page React but I have a problem can't see the link to the video

--- YouTube.WebAPI/Controllers/VideosController.cs
+++ YouTube.WebAPI/Controllers/VideosController.cs
@@ -39,5 +39,29 @@ namespace YouTube.WebAPI.Controllers
 
             return Ok(videos);
         }
+
+
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<DetailesVideoDTO>> GetDetailedVideo(string id)
+        {
+            var videos = await _context.Videos
+                .Include(v => v.Channel)
+                .Select(v => new DetailesVideoDTO
+                {
+                    Id = v.Id,
+                    Title = v.Title,
+                    Description = v.Description,
+                    FilePath = v.FilePath,
+                    ThumbnailPath = v.ThumbnailPath,
+                    UploadDate = v.UploadDate,
+                    ViewCount = v.ViewCount,
+                    LikesCount = v.LikesCount,
+                    ChannelName = v.Channel.Name,
+                   ProfilePicturePath = v.Channel.PicturePath
+                })
+               .FirstOrDefaultAsync(v => v.Id == id);
+
+            return Ok(videos);
+        }
     }
 }
