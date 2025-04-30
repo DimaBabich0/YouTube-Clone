8f8aa368f54a812521ab2e339ee80a54684c28d6
Maks <super_1209@ukr.net>
2025-04-30 19:04:16 +0300

    Add Frontend in VideoPage

--- YouTube.React/src/pages/Video/Video.js
+++ YouTube.React/src/pages/Video/Video.js
@@ -3,6 +3,7 @@ import { useParams, useNavigate } from 'react-router-dom';
 import './Video.css';
 import VideoPreview from '../../components/VideoPreview/VideoPreview';
 
+
 const VideoPage = () => {
   const { id } = useParams();
   const navigate = useNavigate();
@@ -17,10 +18,11 @@ const VideoPage = () => {
 
       try {
         const videoResponse = await fetch(`http://localhost:5103/Videos/${id}`);
-        if (!videoResponse.ok) throw new Error('Видео не найдено');
+        if (!videoResponse.ok) throw new Error('Видео не найдено'); 
 
         const videoData = await videoResponse.json();
         setVideo(videoData);
+        console.log(videoData);
 
         const recommendationsResponse = await fetch(`http://localhost:5103/Videos/Recommended/${id}`);
         if (recommendationsResponse.ok) {
@@ -41,36 +43,151 @@ const VideoPage = () => {
 
   if (!video || !video.filePath) return <div>Видео не найдено</div>;
 
-  return (
-    <div className="video-page">
-      <div className="main-content">
-        <div className="video-container">
-          <div className="video-player-wrapper">
-            <VideoPreview urlVideo={(video.filePath)} /> 
-          </div>
-        </div>
 
-        <div className="video-info">
-          <h1>{video.title}</h1>
-          <p>{video.description}</p>
-          <p>Канал: {video.channelName}</p>
-          <p>Просмотры: {video.viewCount}</p>
-        </div>
+  // const handleRecommendationClick = (id) => {
+  //   const selectedVideo = videos.find(video => video.id === id);
+  //   if (selectedVideo) {
+  //     setVideo(selectedVideo);
+  //     navigate(`/video/${id}`);
+  //   } else {
+  //     console.error(`Video with id ${id} not found`);
+  //   }
+  // };
 
-        <div className="recommendations">
-          <h2>Рекомендованные</h2>
-          <div className="video-grid">
-            {recommendedVideos.map((rec) => (
-              <div key={rec.id} className="recommended-video" onClick={() => navigate(`/video/${rec.id}`)}>
-                <img src={rec.thumbnailPath} alt="thumbnail" />
-                <p>{rec.title}</p>
-              </div>
-            ))}
+
+  if (!video) {
+    return <div>Video not found</div>;
+  }
+
+  const renderVideos = (videos) => {
+    return videos.map((video, index) => (
+
+
+      <div className='item-wrapper' key={index}>
+        <img className='thumbnail' src={video.src} alt="Video thumbnail" />
+        <div className='info-section'>
+          <img className='profile-picture' src={video.src} />
+          <div className='text-section'>
+            <h1 className='video-name'>{video.name}</h1>
+            <p className='channel-name'>{video.channel}</p>
+            <div className='row'>
+              <p className='video-info'>{video.views}</p>
+              <p className='video-info'>{video.date}</p>
+            </div>
           </div>
         </div>
       </div>
+
+    ));
+  };
+
+  return (
+
+    <div className="video-page">
+      <main className="main-content">
+        <div className="video-container">
+          <VideoPreview urlVideo={video.filePath} />
+        </div>
+        <div className="video-title">
+          <h1>{video.title}</h1>
+        </div>
+
+        <div className="author-section">
+          <div className="author-img">
+            <img src={video.profilePicturePath} alt="ENLEO" />
+            <div>
+              <div className="author-name">{video.channelName}</div>
+              {/* <div>{vidoe.}</div> Здесь должно быть количество подписчиков*/}
+            </div>
+          </div>
+          <span className='Subscribe' onClick={() => console.log('Subscribe clicked')}>
+            Subscribe
+          </span>
+          <div className="actions">
+            <span className="action-item" onClick={() => console.log('Likes clicked')}>
+              <img src="/images/icons/heart.svg" alt="Favorite" />
+
+              <p>{video.likeCount} likes</p>
+            </span>
+            <span className="action-item" onClick={() => console.log('Forward clicked')}>
+              <img src="/images/header-images/forward.svg" alt="Forward" />
+              <p>Forward</p>
+            </span>
+            <span className="action-item" onClick={() => console.log('Add to playlist clicked')}>
+              <img className='add-transform' src="/images/icons/add.svg" alt="Add" />
+              <p>Add to playlist</p>
+            </span>
+            <span className="action-item" onClick={() => console.log('More clicked')}>
+              <img src="/images/icons/arrow_back_ios_new.svg" alt="More" />
+              <p>More</p>
+            </span>
+          </div>
+        </div>
+        <div className="description">
+          <p>
+            {video.uploadDate}<br /><br />
+            {video.description}
+          </p>
+          <div className="additional-info">
+            Show more
+            <img src="/images/icons/arrow_back_ios_new.svg" alt="More" />
+          </div>
+        </div>
+
+
+        <div className='section-grid wrapper'>
+          <h1 className='section-name'>Recommended</h1>
+          <section className='video-grid'>
+            {/* {renderVideos(videoData)} */}
+          </section>
+        </div>
+
+
+
+        <div className="comment-wrapper">
+          <p className="comment-profile">Comment</p>
+          <div className="comment-container">
+            <img
+              className="profile-pic"
+              src="/images/header-images/acc.svg"
+              alt="Profile"
+            />
+            <input
+              type="text"
+              className="comment-input"
+              placeholder="Add a comment"
+            />
+            <div className="icons">
+              <span className="icon-smile"><img src="/images/icons/emoji.svg" alt="More" /></span>
+              <span className="icon-at"><img src="/images/icons/Vector.svg" alt="More" /></span>
+              <span className="icon-send"><img src="/images/icons/Frame.svg" alt="More" /></span>
+            </div>
+          </div>
+        </div>
+
+
+
+
+        {/* <CommentList /> */}
+      </main>
+
+      <aside className="recommended-videos">
+        <h2>Videos from this chanel</h2>
+        <ul>
+          {/* {recommendedVideos.map(video => (
+            <li key={video.id} className="recommended-video" onClick={() => handleRecommendationClick(video.id)}>
+              <img src={video.img} alt={video.title} />
+              <div className="video-info">
+                <h3>{video.title}</h3>
+                <p>{video.views}</p>
+              </div>
+            </li>
+          ))} */}
+        </ul>
+      </aside>
     </div>
   );
-};
+}
+
 
 export default VideoPage;
